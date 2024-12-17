976901b3f0e8c16b135b4621a2852b457a100edf
weirdev <weirdev@outlook.com>
2024-12-17 00:01:42 -0500

    Day 15 part 2 completed

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     let inputs = read_inputs("input");
     let sum = gps_sum(inputs);
     println!("GPS Coord Sum: {}", sum);
+
+    let inputs = read_inputs("input");
+    let sum = gps_sum_wide(inputs);
+    println!("GPS Coord Sum (Wide): {}", sum);
 }
 
 fn read_inputs<P>(
@@ -42,6 +46,30 @@ fn lines_to_grid(lines: impl IntoIterator<Item = String>) -> Vec<Vec<char>> {
         .skip(1)
         .map(|l| {
             let mut v: Vec<_> = l.chars().skip(1).collect();
+            v.pop();
+            v
+        })
+        .collect();
+    v.pop();
+    v
+}
+
+fn lines_to_wide_grid(lines: impl IntoIterator<Item = String>) -> Vec<Vec<char>> {
+    // Drop border from grid
+    let mut v: Vec<_> = lines
+        .into_iter()
+        .skip(1)
+        .map(|l| {
+            let mut v: Vec<_> = l
+                .chars()
+                .skip(1)
+                .flat_map(|c| match c {
+                    'O' => ['[', ']'],
+                    '@' => ['@', '.'],
+                    _ => [c, c],
+                })
+                .collect();
+            v.pop();
             v.pop();
             v
         })
@@ -108,6 +136,107 @@ where
     (nx, ny)
 }
 
+fn robot_step_wide<R>(
+    grid: &mut [R],
+    robot_pos: (usize, usize),
+    dx: isize,
+    dy: isize,
+) -> (usize, usize)
+where
+    R: AsMut<[char]> + AsRef<[char]>,
+{
+    let (x, y) = robot_pos;
+    let (nx, ny) = (x as isize + dx, y as isize + dy);
+
+    if robot_push_wide(grid, (nx, ny), dx, dy, false) {
+        robot_push_wide(grid, (nx, ny), dx, dy, true);
+        let (nx, ny) = (nx as usize, ny as usize);
+        grid[ny].as_mut()[nx] = '@';
+        grid[y].as_mut()[x] = '.';
+
+        (nx, ny)
+    } else {
+        (x, y)
+    }
+}
+
+fn robot_push_wide<R>(
+    grid: &mut [R],
+    pushed_into: (isize, isize),
+    dx: isize,
+    dy: isize,
+    do_push: bool,
+) -> bool
+where
+    R: AsMut<[char]> + AsRef<[char]>,
+{
+    let (x, y) = pushed_into;
+
+    if x < 0 || y < 0 || y >= grid.len() as isize || x >= grid[y as usize].as_ref().len() as isize {
+        return false;
+    }
+    let (x, y) = (x as usize, y as usize);
+
+    if dx == 0 {
+        if grid[y].as_ref()[x] == '.' {
+            return true;
+        }
+
+        if grid[y].as_ref()[x] == '#' {
+            return false;
+        }
+
+        // Must be a box
+        let x2 = if grid[y].as_ref()[x] == '[' {
+            x + 1
+        } else {
+            x - 1
+        };
+
+        let (nx, ny) = (x as isize, y as isize + dy);
+        let nx2 = x2 as isize;
+        if robot_push_wide(grid, (nx, ny), dx, dy, do_push)
+            && robot_push_wide(grid, (nx2, ny), dx, dy, do_push)
+        {
+            if do_push {
+                grid[ny as usize].as_mut()[nx as usize] = grid[y].as_ref()[x];
+                grid[ny as usize].as_mut()[nx2 as usize] = grid[y].as_ref()[x2];
+                grid[y].as_mut()[x] = '.';
+                grid[y].as_mut()[x2] = '.';
+            }
+            true
+        } else {
+            false
+        }
+    } else {
+        // dy == 0
+        let mut push_end_x = x as isize;
+        while push_end_x >= 0
+            && push_end_x < grid[y].as_ref().len() as isize
+            && (grid[y].as_ref()[push_end_x as usize] == '['
+                || grid[y].as_ref()[push_end_x as usize] == ']')
+        {
+            push_end_x += dx;
+        }
+
+        if push_end_x < 0
+            || push_end_x >= grid[y].as_ref().len() as isize
+            || grid[y].as_ref()[push_end_x as usize] != '.'
+        {
+            false
+        } else {
+            if do_push {
+                while push_end_x != x as isize {
+                    let prev = push_end_x - dx;
+                    grid[y].as_mut()[push_end_x as usize] = grid[y].as_ref()[prev as usize];
+                    push_end_x = prev;
+                }
+            }
+            true
+        }
+    }
+}
+
 fn dir(c: char) -> (isize, isize) {
     match c {
         '^' => (0, -1),
@@ -125,6 +254,16 @@ where
     let mut robot_pos = robot_pos(grid);
     for d in direction_seq {
         robot_pos = robot_step(grid, robot_pos, dir(d).0, dir(d).1);
+    }
+}
+
+fn execute_robot_run_wide<R>(grid: &mut [R], direction_seq: impl Iterator<Item = char>)
+where
+    R: AsMut<[char]> + AsRef<[char]>,
+{
+    let mut robot_pos = robot_pos(grid);
+    for d in direction_seq {
+        robot_pos = robot_step_wide(grid, robot_pos, dir(d).0, dir(d).1);
     }
 }
 
@@ -150,5 +289,24 @@ fn gps_sum(
         .flat_map(|(y, r)| r.into_iter().enumerate().map(move |(x, c)| (x, y, c)))
         .filter(|(_, _, c)| *c == BOX)
         .map(|(x, y, _)| (x + 1) + ((y + 1) * 100))
+        .sum()
+}
+
+fn gps_sum_wide(
+    inputs: (
+        impl IntoIterator<Item = String>,
+        impl IntoIterator<Item = String>,
+    ),
+) -> usize {
+    let (grid_lines, dir_lines) = inputs;
+    let mut grid = lines_to_wide_grid(grid_lines);
+    let directions = lines_to_dirs(dir_lines);
+    execute_robot_run_wide(&mut grid, directions);
+
+    grid.into_iter()
+        .enumerate()
+        .flat_map(|(y, r)| r.into_iter().enumerate().map(move |(x, c)| (x, y, c)))
+        .filter(|(_, _, c)| *c == '[')
+        .map(|(x, y, _)| (x + 2) + ((y + 1) * 100))
         .sum()
 }
